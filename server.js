e29cc57bf8f4f0b72c3a5aca4b3175cd01b82448
elango1s <elango1@hotmail.com>
2017-06-21 18:55:09 +0530

    initial commit to the project

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+var port = 5000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrYear', () => {
+	return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+})
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method}; ${req.url} \n`
+	console.log(log);
+	fs.appendFile('server.log', log, (err) => {
+		if(err){
+			console.log("Could not append to log file");
+		}
+	})
+	next();
+});
+
+app.get('/index/html', (req,res) => {
+	res.send("<h3>Hello Express </h3>:-)");
+});
+
+app.get('/about', (req,res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('', (req,res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		pageMessage: 'Welcome to my world of express and handlebars'
+	});
+});
+
+app.get('/index/json', (req,res) => {
+	res.send({
+		name: 'Elango',
+		age: 'still young'
+		})
+});
+
+app.listen(5000, () => {
+	console.log(`server is up on ${port} and on dir: ${__dirname}`);
+});
